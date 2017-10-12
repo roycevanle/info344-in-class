fb3c35b1b2fa1917fece85186bc584cad20f25a7
Royce Le <roycele@D-108-179-175-212.dhcp4.washington.edu>
2017-10-12 15:14:17 -0700

    moved things around

--- zipsvr/handlers/zips.go
+++ zipsvr/handlers/zips.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/roycevanle/info344-a17/info344-in-class/zipsvr/models"
+	"github.com/roycevanle/info344-in-class/zipsvr/models"
 )
 
 type CityHandler struct {
